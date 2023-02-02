8601fe1d55eac2074f42a407d15cb82b6530c2b3
oscar <oscar@DESKTOP-8I5T1J1>
2023-02-02 10:03:09 +0100

    “Se ha creado la rama clase nueva”.

---
+++ src/main/java/EjercicioGit/NuevaClase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjercicioGit;
+
+/**
+ *
+ * @author oscar
+ */
+public class NuevaClase {
+public static void main ( String[] args){
+System.out.println("En la rama. Se crea la clase nueva");
+}
+}
